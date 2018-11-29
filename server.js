aac69b5471427362db059bbcd771e954e384c8de
Daniel-Alpha <45221469+Daniel-Alpha@users.noreply.github.com>
2018-11-29 11:56:07 +0100

    server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const port = 3001
+http.createServer((req, res) => {
+res.writeHead(200, {'Content-Type': 'text/plain'})
+res.end('Hello World\n')
+}).listen(port,() => {
+console.log(`server running at http://localhost:${port}`)
+})
+
