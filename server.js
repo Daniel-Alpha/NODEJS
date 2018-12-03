7e6e828cccc24750f7fcab45473498ad41c063e0
Daniel-Alpha <45221469+Daniel-Alpha@users.noreply.github.com>
2018-12-03 18:01:57 +0100

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const port = 3001
-http.createServer((req, res) => {
-res.writeHead(200, {'Content-Type': 'text/plain'})
-res.end('Hello World\n')
-}).listen(port,() => {
-console.log(`server running at http://localhost:${port}`)
-})
-
